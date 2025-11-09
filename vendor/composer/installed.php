be84ee622111eeb2988ef385ca11b0d3a3a60394
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-11-09 13:54:11 -0500

    release

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'drupal/recommended-project',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'e60f02e802c04ec6fc914b862ba191b15f71396a',
+        'reference' => '1c57adfaae57adae1036c5355e997cb9c989dfaf',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -415,6 +415,15 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'drupal/file_delete' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => '3.0.0',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../web/modules/contrib/file_delete',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'drupal/inline_entity_form' => array(
             'pretty_version' => '3.0.0-rc21',
             'version' => '3.0.0.0-RC21',
@@ -445,7 +454,7 @@
         'drupal/recommended-project' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'e60f02e802c04ec6fc914b862ba191b15f71396a',
+            'reference' => '1c57adfaae57adae1036c5355e997cb9c989dfaf',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
