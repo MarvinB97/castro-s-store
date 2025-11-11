9743cd08c34620c9401116cd5a7a8079170bb541
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-11-10 19:42:41 -0500

    start

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,10 +5,10 @@ echo '⏳ Esperando conexión a la base de datos...'
 sleep 10
 
 echo '✅ Importando configuración con drush cim -y...'
-vendor/bin/drush cim -y || true
+./vendor/bin/drush cim -y || true
 
 echo '♻️ Limpiando caché...'
-vendor/bin/drush cr || true
+./vendor/bin/drush cr || true
 
 echo '🚀 Iniciando servidor Drupal...'
 php -d memory_limit=512M -S 0.0.0.0:${PORT:-8080} -t web
